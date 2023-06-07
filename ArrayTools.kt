3ccf65b6e81cb37f29cc5bc1496816bb8d50dec5
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-07 11:15:21 -0400

    Created Funtion and variable for average of array

---
+++ ArrayTools.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("hello world)
+}
+// Averages the element of an integer array and output the avegrages as double
+fun averageArray(inputArray: Array<Int>): Double{
+    var sum = 0.0
+ 
+    
+}

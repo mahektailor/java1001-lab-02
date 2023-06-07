a6404bdeac4e5575b8c325845ceabea059b308e3
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-07 11:34:43 -0400

    Sum average of Array

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,9 +1,11 @@
 fun main() {
-    println("hello world)
+    println(averageArray(arrayOf(1,2,3,4,5)))
 }
-// Averages the element of an integer array and output the avegrages as double
-fun averageArray(inputArray: Array<Int>): Double{
+fun averageArray(inputArray: Array<Int>): Double {
+    // Creating Variable sum to find summation of array
     var sum = 0.0
- 
-    
+    for (number in inputArray){
+        sum +=number
+    }
+    return sum/inputArray.size
 }

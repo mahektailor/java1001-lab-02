dfbae6fe4fac6d350e1b29db03e06c596d03cd91
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-10 01:24:33 -0400

    array and function to find string isin that array

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,5 +1,11 @@
 fun main() {
   averageArrayTester()
+  val arrayNumber = arrayOf(1, 2, 3, 4, 5)
+  // serching string "3" in array numbers 
+    val searchValue = 3
+    // variable contains  
+    val containsValue = arrayContains(arrayNumber, searchValue)
+    println("Array contains $searchValue: $containsValue")
 //
 }
 fun averageArray(inputArray: Array<Int>): Double {
@@ -19,4 +25,8 @@ fun averageArrayTester(){
     print ("this is sum of null array  :")
     println(averageArray(arrayOf()))
 }
-
+// added function arryContains to check if that is present or not
+// using boolen to return answer true or false
+fun <T> arrayContains(array: Array<T>, searchValue: T): Boolean {
+    return searchValue in array
+}
